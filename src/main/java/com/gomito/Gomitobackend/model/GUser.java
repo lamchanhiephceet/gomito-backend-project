3a9b91983b988cbb2233701fd4f7a3957f7a7961
Hiep Nguyen <lamchanhiephceet@gmail.com>
2021-07-19 14:10:29 +0700

    add application_local

--- src/main/java/com/gomito/Gomitobackend/model/GUser.java
+++ src/main/java/com/gomito/Gomitobackend/model/GUser.java
@@ -14,7 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "gomito_users")
 public class GUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
