a4de5730221eaa930e104f64a7017b0bd84c1a28
Hiep Nguyen <lamchanhiephceet@gmail.com>
2021-07-20 15:24:22 +0700

    JavaMailSender update

--- src/main/java/com/gomito/Gomitobackend/GomitoBackendApplication.java
+++ src/main/java/com/gomito/Gomitobackend/GomitoBackendApplication.java
@@ -2,10 +2,32 @@ package com.gomito.Gomitobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class GomitoBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GomitoBackendApplication.class, args);
+	}
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+
+		mailSender.setUsername("my.gmail@gmail.com");
+		mailSender.setPassword("password");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
 	}
 }
