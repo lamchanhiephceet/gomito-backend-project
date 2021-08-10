6484349f390470bf333f25ca8e82edf892baf712
Hiep Nguyen <lamchanhiephceet@gmail.com>
2021-08-10 16:50:46 +0700

    authService error fix

--- src/main/java/com/gomito/Gomitobackend/service/AuthService.java
+++ src/main/java/com/gomito/Gomitobackend/service/AuthService.java
@@ -48,8 +48,8 @@ public class AuthService {
         gUser.setUsername(signUpRequest.getUsername());
         gUser.setEmail(signUpRequest.getEmail());
         gUser.setPassword(encodePassword(signUpRequest.getPassword()));
-        gUser.setEnabled(false);
-//        gUser.setEnabled(true);
+//        gUser.setEnabled(false);
+        gUser.setEnabled(true);
         gUserRepository.save(gUser);
 
         String token = generateVerificationToken(gUser);
